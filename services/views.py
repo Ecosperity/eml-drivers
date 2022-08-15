26f522cc37a414b6d198233f61a122b583246afe
Dilawar Sumra <admin@ecosperityindia.com>
2022-08-15 20:13:45 +0530

    Search Filter Added

--- services/views.py
+++ services/views.py
@@ -1,8 +1,10 @@
 from .models import Driver
 from .serializers import DriverSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, filters
 # Create your views here.
 
 class DriverList(ListAPIView):
+  search_fields = ['name', 'complaint']
+  filter_backends = (filters.SearchFilter)
   queryset = Driver.objects.all()
   serializer_class = DriverSerializer
