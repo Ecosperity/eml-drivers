feb0d6cd961d10b07ef1975782215f229cbc0b0f
Dilawar Sumra <admin@ecosperityindia.com>
2022-08-15 15:29:15 +0530

    Second Commit

--- emlbackend/settings.py
+++ emlbackend/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
+
+SECRET_KEY = 'django-insecure-9w!o_en$6p38uvmln^2b(+x3bp*0qzqwd9@_g=4!a)*@-^i3#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
