40fda6cee3fa0b51ca69b302daa86e1342825696
Dilawar Sumra <admin@ecosperityindia.com>
2022-08-15 20:23:47 +0530

    Search Filter Added

--- services/views.py
+++ services/views.py
@@ -1,10 +1,11 @@
+from django.shortcuts import render
 from .models import Driver
 from .serializers import DriverSerializer
-from rest_framework.generics import ListAPIView
-from rest_framework import filters
-# Create your views here.
+# from rest_framework.generics import ListAPIView
+from rest_framework import generics, filters
 
-class DriverList(ListAPIView):
+
+class DriverList(generics.ListAPIView):
   search_fields = ['name', 'complaint']
   filter_backends = (filters.SearchFilter)
   queryset = Driver.objects.all()
