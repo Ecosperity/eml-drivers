0613bed93c4a90e0ba87d6a60f02090548e5e32f
Dilawar Sumra <admin@ecosperityindia.com>
2022-08-15 21:38:57 +0530

    Search Filter Added

--- services/views.py
+++ services/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Driver
 from .serializers import DriverSerializer
-from rest_framework import generics, filters
+from rest_framework import generics
 
-# from django_filters import rest_framework as filters
-# from django_filters.rest_framework import DjangoFilterBackend
-# from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 
