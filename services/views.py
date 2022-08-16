af0ba96bb39656ebb6032dcf05d04e84cf3fca23
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-16 17:50:51 +0530

    Id filter added

--- services/views.py
+++ services/views.py
@@ -9,7 +9,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 
-class DriverList(generics.ListCreateAPIView):
+def DriverList():
   search_fields = ['name', 'complaint']
   filter_backends = (SearchFilter, filters.DjangoFilterBackend)
   queryset = Driver.objects.filter(id = 17)
