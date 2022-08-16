15be9480df25da612a924301913c4f40aaa4252e
Dilawar Sumra <admin@ecosperityindia.com>
2022-08-16 22:11:41 +0530

    Test 16_08

--- services/urls.py
+++ services/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from services import views
 
 urlpatterns = [
-    path('driver/', views.DriverList.as_view()),
+    path('driver/<fid>', views.DriverList),
 ]
--- services/views.py
+++ services/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 from .models import Driver
 from .serializers import DriverSerializer
 from rest_framework import generics
+from django.http import JsonResponse
 
-from django_filters import rest_framework as filters
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
+# from django_filters import rest_framework as filters
+# from django_filters.rest_framework import DjangoFilterBackend
+# from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 
-class DriverList(generics.ListCreateAPIView):
-  search_fields = ['name', 'complaint']
-  filter_backends = (SearchFilter, filters.DjangoFilterBackend)
-  queryset = Driver.objects.all()
-  serializer_class = DriverSerializer
+# class DriverList(generics.ListCreateAPIView):
+#   search_fields = ['name', 'complaint']
+#   filter_backends = (SearchFilter, filters.DjangoFilterBackend)
+#   queryset = Driver.objects.all()
+#   serializer_class = DriverSerializer
+
+def DriverList(request, fid):
+  d = Driver.objects.filter(id=fid)
+  data = list(d.values())
+  return JsonResponse(data, safe=False)
