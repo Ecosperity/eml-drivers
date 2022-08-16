e3452410426a28e3459dc6e485450fe0894e26b0
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-16 19:13:00 +0530

    Id filter added

--- services/views.py
+++ services/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import Driver
 from .serializers import DriverSerializer
 from rest_framework import generics
-from services.serializers import DriverSerializer
 
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
@@ -11,8 +10,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class DriverList(generics.ListCreateAPIView):
-  def getData(self, **kwargs):
-    search_fields = ['name', 'complaint']
-    filter_backends = (SearchFilter, filters.DjangoFilterBackend)
-    queryset = Driver.objects.get(id=kwargs['fid'])
-    serializer_class = DriverSerializer
+  search_fields = ['name', 'complaint']
+  filter_backends = (SearchFilter, filters.DjangoFilterBackend)
+  queryset = Driver.objects.all()
+  serializer_class = DriverSerializer
