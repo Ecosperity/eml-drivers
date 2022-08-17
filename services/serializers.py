ebd30d492128068976d12b9c33cbe6ef7611c7bf
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-17 14:17:45 +0530

    test

--- services/serializers.py
+++ services/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Driver
 
-class DriverSerializer(serializers.ModelSerializer):
+class DriverSerializer(serializers.Serializer):
   class Meta:
     model = Driver
-    fields = ['id', 'name', 'complaint']
+    fields = '__all__'
--- services/views.py
+++ services/views.py
@@ -1,21 +1,25 @@
-from http.client import HTTPResponse
-from urllib import request
+# from urllib import request
 from .models import Driver
 from .serializers import DriverSerializer
-from rest_framework import generics
-from django.views import View
-from django.http import JsonResponse
+# from rest_framework import generics
+# from django.views import View
+# from django.http import JsonResponse
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
-from django_filters import rest_framework as filters
+# from django_filters import rest_framework as filters
 # from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
+# from rest_framework.filters import SearchFilter, OrderingFilter
 
 from django.http import JsonResponse
 
 def getData(request):
   set = Driver.objects.all()
-  data = list(set.values())
-  return JsonResponse(data, safe=False)
+  serializer = DriverSerializer(set)
+  json_data = JSONRenderer().render(serializer.data)
+  return HttpResponse(json_data, content_type='application/json')
+  # data = list(set.values())
+  # return JsonResponse(data, safe=False)
 
 # class DriverList(generics.ListCreateAPIView):
 #   search_fields = ['name', 'complaint']
