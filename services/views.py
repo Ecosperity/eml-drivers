b5d2b613195be60a4f78dbe343dcba2ce36b4c19
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-17 15:37:47 +0530

    test

--- services/views.py
+++ services/views.py
@@ -16,3 +16,15 @@ def DriverApi(request):
       serializer = DriverSerializer(data)
       json_data = JSONRenderer().render(serializer.data)
       return HttpResponse(json_data, content_type='application/json')
+  
+  if (request.method == "POST"):
+    json_data = request.body
+    stream = io.BytesIO(json_data)
+    python_data = JSONParser().parse(stream)
+    serializer = DriverSerializer(data=python_data)
+    if serializer.is_valid():
+      serializer.save()
+      res = {'msg': 'Data Created Successfully'}
+      json_data = JSONRenderer().render(res)
+      return HttpResponse(json_data, content_type='application/json')
+    return HttpResponse(JSONRenderer().render(serializer.errors), content_type='application/json')
