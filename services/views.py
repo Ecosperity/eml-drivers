5c9f9b3fe672b6afc2b89ba1365256bcd8dc7eb5
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-17 13:36:04 +0530

    test

--- services/views.py
+++ services/views.py
@@ -1,24 +1,24 @@
 from http.client import HTTPResponse
 from urllib import request
 from .models import Driver
-# from .serializers import DriverSerializer
-# from rest_framework import generics
+from .serializers import DriverSerializer
+from rest_framework import generics
 from django.views import View
 from django.http import JsonResponse
 
-# from django_filters import rest_framework as filters
+from django_filters import rest_framework as filters
 # from django_filters.rest_framework import DjangoFilterBackend
-# from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from django.http import JsonResponse
 
-def getData(request):
-  set = Driver.objects.all()
-  data = list(set.values())
-  return JsonResponse(data, safe=False)
+# def getData(request):
+#   set = Driver.objects.all()
+#   data = list(set.values())
+#   return JsonResponse(data, safe=False)
 
-# class DriverList(View):
-  # search_fields = ['name', 'complaint']
-  # filter_backends = (SearchFilter, filters.DjangoFilterBackend)
-  # queryset = Driver.objects.all()
-  # serializer_class = DriverSerializer
+class DriverList(generics.ListCreateAPIView):
+  search_fields = ['name', 'complaint']
+  filter_backends = (SearchFilter, filters.DjangoFilterBackend)
+  queryset = Driver.objects.all()
+  serializer_class = DriverSerializer
