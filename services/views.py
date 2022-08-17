e63b57e07c6927d4097702f0b78c8342bae3b6b4
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-17 12:19:19 +0530

    test

--- services/views.py
+++ services/views.py
@@ -16,11 +16,12 @@ from django.http import JsonResponse
 class DriverList(View):
   # search_fields = ['name', 'complaint']
   # filter_backends = (SearchFilter, filters.DjangoFilterBackend)
-  queryset = Driver.objects.all()
+  # queryset = Driver.objects.all()
   # serializer_class = DriverSerializer
 
   def getData(self):
-    data = list(self.queryset.values())
+    set = Driver.objects.all()
+    data = list(set.values())
     return JsonResponse(data, safe=False)
 
 # def GetAll(request):
