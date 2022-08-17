b548059c278063a1e2f9575b22a2b956ab8898e6
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-17 16:55:27 +0530

    test

--- services/views.py
+++ services/views.py
@@ -6,16 +6,23 @@ from django.http import HttpResponse
 import io
 
 def DriverApi(request):
+  # if (request.method == "GET"):
+  #   json_data = request.body
+  #   stream = io.BytesIO(json_data)
+  #   python_data = JSONParser().parse(stream)
+  #   fid = python_data.get('id')
+  #   if id is not None:
+  #     data = Driver.objects.get(id=fid)
+  #     serializer = DriverSerializer(data)
+  #     json_data = JSONRenderer().render(serializer.data)
+  #     return HttpResponse(json_data, content_type='application/json')
+
   if (request.method == "GET"):
-    json_data = request.body
-    stream = io.BytesIO(json_data)
-    python_data = JSONParser().parse(stream)
-    fid = python_data.get('id')
-    if id is not None:
-      data = Driver.objects.get(id=fid)
-      serializer = DriverSerializer(data)
-      json_data = JSONRenderer().render(serializer.data)
-      return HttpResponse(json_data, content_type='application/json')
+    data = Driver.objects.get.all()
+    data_list = list(data.values())
+    serializer = DriverSerializer(data_list)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
   
   if (request.method == "POST"):
     json_data = request.body
