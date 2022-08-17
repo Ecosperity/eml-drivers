36b8880130bbae47bf5b00bee247dfea2f493576
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-17 11:04:21 +0530

    Test 17-08

--- services/views.py
+++ services/views.py
@@ -6,28 +6,25 @@ from .serializers import DriverSerializer
 from rest_framework import generics
 from django.http import JsonResponse
 from django.core.serializers.json import DjangoJSONEncoder
-# from django.views.decorators.csrf import csrf_exempt, requires_csrf_token
 
-# from django_filters import rest_framework as filters
-# from django_filters.rest_framework import DjangoFilterBackend
-# from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from django.http import JsonResponse
 
-# class DriverList(generics.ListCreateAPIView):
-#   search_fields = ['name', 'complaint']
-#   filter_backends = (SearchFilter, filters.DjangoFilterBackend)
-#   queryset = Driver.objects.all()
-#   serializer_class = DriverSerializer
+class DriverList(generics.ListCreateAPIView):
+  search_fields = ['name', 'complaint']
+  filter_backends = (SearchFilter, filters.DjangoFilterBackend)
+  queryset = Driver.objects.all()
+  serializer_class = DriverSerializer
 
-# @requires_csrf_token
-def GetAll(request):
-  d = Driver.objects.all()
-  data = list(d.values())
-  return JsonResponse(data, encoder=DjangoJSONEncoder, safe=False)
+# def GetAll(request):
+#   d = Driver.objects.all()
+#   data = list(d.values())
+#   return JsonResponse(data, encoder=DjangoJSONEncoder, safe=False)
 
-# @requires_csrf_token
-def FilterById(request, fid):
-  d = Driver.objects.filter(id=fid)
-  data = list(d.values())
-  return JsonResponse(data, encoder=DjangoJSONEncoder, safe=False)
+# def FilterById(request, fid):
+#   d = Driver.objects.filter(id=fid)
+#   data = list(d.values())
+#   return JsonResponse(data, encoder=DjangoJSONEncoder, safe=False)
