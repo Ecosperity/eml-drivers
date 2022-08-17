528c2518cc7ed684969f37058c3d1adc6c8d2139
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-17 14:24:46 +0530

    test

--- services/urls.py
+++ services/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from services import views
 
 urlpatterns = [
-    path('driver/', views.DriverList.as_view()),
-    path('driverd/<fid>/', views.DriverList.as_view()),
+    path('driver/', views.getData),
 ]
