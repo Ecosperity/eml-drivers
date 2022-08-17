3603a59a9dd3e46408e247e538347aa2d2dd154f
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-17 10:48:42 +0530

    Test 17-08

--- services/views.py
+++ services/views.py
@@ -5,9 +5,9 @@ from .models import Driver
 from .serializers import DriverSerializer
 from rest_framework import generics
 
-from django_filters import rest_framework as filters
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
+# from django_filters import rest_framework as filters
+# from django_filters.rest_framework import DjangoFilterBackend
+# from rest_framework.filters import SearchFilter, OrderingFilter
 
 from django.http import JsonResponse
 
