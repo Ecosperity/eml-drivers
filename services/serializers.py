e34b8efaefdc0cdee66333825083a1091eec8ae6
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-17 16:07:51 +0530

    test

--- services/serializers.py
+++ services/serializers.py
@@ -6,9 +6,9 @@ class DriverSerializer(serializers.ModelSerializer):
     model = Driver
     fields = '__all__'
 
-    def update(self, instance, validated_data):
-      instance.id = validated_data.get('id', instance.id)
-      instance.name = validated_data.get('name', instance.name)
-      instance.complaint = validated_data.get('complaint', instance.complaint)
-      instance.save()
-      return instance
+def update(self, instance, validated_data):
+  instance.id = validated_data.get('id', instance.id)
+  instance.name = validated_data.get('name', instance.name)
+  instance.complaint = validated_data.get('complaint', instance.complaint)
+  instance.save()
+  return instance
