8ab97402ccdbabc956b378ebf8fd69652c7d5e3d
Ecosperity Admin <admin@ecosperityindia.com>
2022-08-18 15:26:16 +0530

    Test

--- services/models.py
+++ services/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Registration(models.Model):
   Name = models.CharField(max_length=30)
-  Age = models.PositiveIntegerField
+  Age = models.PositiveIntegerField(default=0)
   State = models.CharField(max_length=30)
   City = models.CharField(max_length=30)
   phoneNo = models.CharField(max_length=20)
@@ -11,7 +11,7 @@ class Registration(models.Model):
   DoM = models.CharField(max_length=30)
   DoR = models.CharField(max_length=30)
   DoIns = models.CharField(max_length=30)
-  InsDuration = models.PositiveIntegerField
+  InsDuration = models.PositiveIntegerField(default=0)
   ChassisNo = models.CharField(max_length=30)
   modelNo = models.CharField(max_length=30)
 
