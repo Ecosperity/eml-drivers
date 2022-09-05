e51ee73a83d3a5454e1fdbfb37a9a57fd45ac56c
Ecosperity Admin <admin@ecosperityindia.com>
2022-09-05 15:27:21 +0530

    first commit

---
+++ services/consumers.py
@@ -0,0 +1,23 @@
+from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.exceptions import StopConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+import json
+
+class mySock(WebsocketConsumer):
+    def connect(self):
+        self.room_name = 'EML'
+        self.room_group_name = 'EML ROOM'
+        self.accept()
+        self.send(
+            text_data=json.dumps({'status': 'Connected to Socket'})
+        )
+        
+    def receive(self, text_data):
+        print(text_data)
+        self.send(
+            text_data=json.dumps({'status': 'We got you'})
+        )
+
+    def disconnect(self, code):
+        pass
