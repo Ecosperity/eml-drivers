10b088d1eb953aa5e55a8f7edd15626aec2533ae
Ecosperity Admin <admin@ecosperityindia.com>
2022-09-17 12:57:19 +0530

    Test

--- services/consumers.py
+++ services/consumers.py
@@ -1,8 +1,35 @@
 from channels.consumer import SyncConsumer, AsyncConsumer
 from channels.exceptions import StopConsumer
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync, sync_to_async
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 import json
+from .models import *
+
+class CompConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name='Complaint Data'
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        await self.accept()
+    
+    async def disconnect(self,close_code):
+        pass
+
+    async def receive(self,text_data):
+        # print (text_data)
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type':'send_order',
+                'value':text_data,
+            }
+        )
+
+    async def send_order(self,event):
+        print (event['value'])
+        await self.send(event['value'])
 
 class mySock(WebsocketConsumer):
     def connect(self):
--- services/routing.py
+++ services/routing.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('', consumers.mySock.as_asgi())
+    path('test', consumers.mySock.as_asgi())
+    path('comp', consumers.CompConsumer.as_asgi())
 ]
