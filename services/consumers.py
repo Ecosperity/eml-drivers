64254ee6cc535282de76563ef6ecead274ed3c51
Ecosperity Admin <admin@ecosperityindia.com>
2022-09-17 15:02:39 +0530

    Test

--- services/consumers.py
+++ services/consumers.py
@@ -5,31 +5,31 @@ from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 import json
 from .models import *
 
-class CompConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.group_name='Complaint Data'
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name
-        )
-        await self.accept()
+# class CompConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         self.group_name='Complaint Data'
+#         await self.channel_layer.group_add(
+#             self.group_name,
+#             self.channel_name
+#         )
+#         await self.accept()
     
-    async def disconnect(self,close_code):
-        pass
+#     async def disconnect(self,close_code):
+#         pass
 
-    async def receive(self,text_data):
-        # print (text_data)
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                'type':'send_order',
-                'value':text_data,
-            }
-        )
+#     async def receive(self,text_data):
+#         # print (text_data)
+#         await self.channel_layer.group_send(
+#             self.group_name,
+#             {
+#                 'type':'send_order',
+#                 'value':text_data,
+#             }
+#         )
 
-    async def send_order(self,event):
-        print (event['value'])
-        await self.send(event['value'])
+#     async def send_order(self,event):
+#         print (event['value'])
+#         await self.send(event['value'])
 
 class mySock(WebsocketConsumer):
     def connect(self):
